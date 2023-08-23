32843c572e678b05a88710300b7f4ade73af1547
trickrenzgarcia <trickrenzgarcia@gmail.com>
2023-08-23 19:30:57 +0800

    asdawd

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { ThemeProvider } from '@/components/providers/theme-provider'
 import './globals.css'
 import type { Metadata } from 'next'
@@ -7,6 +9,7 @@ import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
 import Tabs from '@/components/Tabs'
 import { TabsContent } from '@/components/ui/tabs'
+import { usePathname } from 'next/navigation'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -26,18 +29,21 @@ type Props = {
 }
 
 export default function RootLayout({ children, employees, ojts, flexible }: Props) {
-
+  const pathname = usePathname()
+  console.log(pathname)
   return (
     <html lang="en">
       <body className={inter.className + " container"}>
         <ThemeProvider attribute='class' defaultTheme='light' enableSystem>
           <AuthProvider>
             <Navbar />
-            <Tabs>
-              <TabsContent value='employees'>{employees}</TabsContent>
-              <TabsContent value='ojts'>{ojts}</TabsContent>
-              <TabsContent value='flexible'>{flexible}</TabsContent>
-            </Tabs>
+            {pathname === '/' && (
+              <Tabs>
+                <TabsContent value='employees'>{employees}</TabsContent>
+                <TabsContent value='ojts'>{ojts}</TabsContent>
+                <TabsContent value='flexible'>{flexible}</TabsContent>
+              </Tabs>
+            )}
             {children}
             <Footer year='2023' corporation='Brojava'/>
           </AuthProvider>
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import Tabs from '@/components/Tabs';
+import { TabsContent } from '@/components/ui/tabs';
+
 export default async function Home() {
   
   return (
-    <></>
+    <>
+      
+    </>
   )
 }
 
